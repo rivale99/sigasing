b08a49c28acca6d3966818ccace520843c1bde6e
unknown <rivalervanda@gmail.com>
2022-01-30 23:52:40 +0800

    Pertemuan 1

---
+++ partials/footer.php
@@ -0,0 +1,9 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-block">
+      <strong>Version</strong> 0.1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; <?= date('Y'); ?> <a href="javascript:void(0);">Rival Ervanda</a>.</strong> All rights reserved.
+  </footer>
